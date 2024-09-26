75b5d0754bb1d9e2df8ca38f78eef5e8ecf05b88
nguyentramy1 <168984915+nguyentramy1@users.noreply.github.com>
2024-09-26 16:04:31 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,3 +15,40 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+import React, { useState } from 'react';
+import { createPortal } from 'react-dom';
+import ModalContent from './Modal-content.tsx';
+import Button from "../Button/Button.tsx";
+
+const PortalExample: React.FC = () => {
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  return (
+    <>
+      <Button 
+        content="Show modal using a portal" 
+        onClick={() => setShowModal(true)} 
+      />
+      {showModal && createPortal(
+        <ModalContent onClose={() => setShowModal(false)} label='Thêm mới bảng lương'/>,
+        document.body
+      )}
+    </>
+  );
+};
+
+export default PortalExample;
+
+
+
+
+
+
+
+
